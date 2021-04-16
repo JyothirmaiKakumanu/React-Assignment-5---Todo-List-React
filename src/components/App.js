9164624df771653a824c692c1cba753ee58e1ff1
JyothirmaiKakumanu <76103448+JyothirmaiKakumanu@users.noreply.github.com>
2021-04-16 18:21:26 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import List from "./DisplayTasks";
 function App() {
   const [task, settask] = useState({ task_name: "", edit: false });
   const [list, setlist] = useState([]);
+  const [temp_text, settemtext] = useState("");
+  const ip = useRef(null);
 
   const handleonchange = (e) => {
     //setText(e.target.value)
@@ -21,6 +23,7 @@ function App() {
     if (task.task_name != "") {
       setlist((list) => [...list, task]);
     }
+    settask({ task_name: "", edit: false });
   };
   const delete_todo_item = (element_tobe_deleted, i) => {
     setlist(list.filter((t, index) => Number(i) != index));
@@ -28,7 +31,7 @@ function App() {
   const edit_todo_item = (element_tobe_edited, i) => {
     var temp = list;
     temp.map((ele, index) => {
-      if (ele.task_name == element_tobe_edited) {
+      if (index === i) {
         ele.edit = true;
       }
     });
@@ -37,24 +40,19 @@ function App() {
 
   const handle_edit = (text, index) => {
     //console.log(index);
-    var obj = list;
-    obj[index].task_name = text;
-
-    setlist((list) => [...list], obj);
+    settemtext(text);
   };
   const handle_edit_save_button = (i) => {
     //setlist(list=> [...list] , obj)
     var obj2 = list;
 
-    if (obj2[i].task_name != "") {
+    if (temp_text.length > 0) {
+      obj2[i].task_name = temp_text;
       obj2[i].edit = false;
 
-      setlist((list) => [...list], obj2);
-    } else {
-      let temp = obj2.filter((inedx) => {
-        inedx != i;
-      });
-      setlist(temp);
+      setlist(obj2);
+      settemtext("");
+      //	console.log(list.length);
     }
   };
 
